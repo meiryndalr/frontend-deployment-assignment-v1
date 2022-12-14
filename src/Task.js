12a56b394cc67cdf2123f358935e004feecadfb2
meiryndalr <107854505+meiryndalr@users.noreply.github.com>
2022-12-14 13:52:04 +0700

    push all file

---
+++ src/Task.js
@@ -0,0 +1,5 @@
+const url = "YOURDEPLOYURL" // TODO: replace this
+const studentName = "Meirynda Lastika Rahimsyah" // TODO: replace this
+const studentId = "FE2692568" // TODO: replace this
+
+export { url, studentName, studentId }
---
+++ src/routes/Photos.jsx
@@ -0,0 +1,104 @@
+import { useEffect } from "react";
+import { useState } from "react";
+import Card from "../components/Card";
+
+const Photos = () => {
+  const [photos, setPhotos] = useState([]);
+  const [sort, setSort] = useState("asc");
+  const [submited, setSubmited] = useState("");
+  const [search, setSearch] = useState("");
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(null);
+
+  const deletePhoto = (id) => {
+    fetch(`http://gallery-app-server.vercel.app/photos/${id}`,{
+      method: "DELETE",
+      
+    })
+    .then((response) => {
+    return response.json()})
+    .then(foto => {
+      setPhotos(foto => {
+        return foto.filter(photo => photo.id !== id)
+      })
+      
+    })
+  };
+
+  useEffect(() => {
+
+    // TODO: answer here
+    fetch(`http://gallery-app-server.vercel.app/photos?_order=${sort}&_sort=id&q=${search}`)
+    .then((response) => response.json())
+    .then((json) => {
+      setPhotos(json)
+      setLoading(false)
+    })
+  }, [sort, submited]);
+
+  useEffect(() => {
+    fetch("http://gallery-app-server.vercel.app/photos")
+    .then((response) => response.json())
+    .then((json) => {
+      setPhotos(json)
+      setLoading(false);
+    })
+    
+  }, []);
+
+  if (error) return <h1 style={{ width: "100%", textAlign: "center", marginTop: "20px" }} >Error!</h1>;
+
+  return (
+    <>
+      <div className="container">
+        <div className="options">
+          <select
+            onChange={(e) => setSort(e.target.value)}
+            data-testid="sort"
+            className="form-select"
+            style={{}}
+          >
+            <option value="asc">Ascending</option>
+            <option value="desc">Descending</option>
+          </select>
+          <form
+            onSubmit={(e) => {
+              e.preventDefault();
+              setSubmited(search);
+            }}
+          >
+            <input
+              type="text"
+              data-testid="search"
+              onChange={(e) => setSearch(e.target.value)}
+              className="form-input"
+            />
+            <input
+              type="submit"
+              value="Search"
+              data-testid="submit"
+              className="form-btn"
+            />
+          </form>
+        </div>
+        <div className="content">
+          {loading ? (
+            <h1
+              style={{ width: "100%", textAlign: "center", marginTop: "20px" }}
+            >
+              Loading...
+            </h1>
+          ) : (
+            photos.map((photo) => {
+              return (
+                <Card key={photo.id} photo={photo} deletePhoto={deletePhoto} />
+              );
+            })
+          )}
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Photos;
